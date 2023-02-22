b813478bd7dea187c3f25e4f2aa9a36442975765
앵가리 <>
2023-02-23 00:54:39 +0900

    환경변수 처리 / 데이터베이스 연결: config / typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { Users } from './entities/users.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [
+          Users
+      ],
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { Users } from 'src/entities/users.entity';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([Users])],
+    controllers: [UsersController],
+    providers: [UsersService],
+})
 export class UsersModule {}
