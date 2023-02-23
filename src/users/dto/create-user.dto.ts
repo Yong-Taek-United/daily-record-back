403f7af7111440f9ee7f7947eaa1627d1e169afc
앵가리 <>
2023-02-24 02:04:05 +0900

    회원정보 수정 기능 추가: 암호 hash

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -8,7 +8,7 @@ export class CreateUserDto {
   readonly username: string;
 
   @IsString()
-  readonly password: string;
+  password: string;
 
   @IsString()
   readonly password2: string;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -31,31 +31,32 @@ export class UsersService {
         }
 
         const hashedPassword = await bcrypt.hash (password, 10);
-        const user: object = {
-            email: email,
-            username: username,
-            password: hashedPassword
-        }
-        this.usersRepository.save(user);
-        return {Success: true, statusCode: 201, message: '회원가입이 완료되었습니다.'}
+        userData.password = hashedPassword;
+        this.usersRepository.save(userData);
+        return {Success: true, statusCode: 201, message: '회원가입이 완료되었습니다.'};
     }
 
     // 회원 조회
     async get(id: number) {
         const user = await this.usersRepository.findOne({where:{id}});
-        return{Success: true, statusCode: 201, message: '회원 조회가 완료되었습니다.', userData: user}
+        return{Success: true, statusCode: 201, message: '회원 조회가 완료되었습니다.', userData: user};
     }
 
     // 회원정보 수정
     async update(id: number, userData: UpdateUserDto) {
+        console.log(userData)
+        if(userData.password) {
+            const hashedPassword = await bcrypt.hash (userData.password, 10);
+            userData.password = hashedPassword;
+        }
         await this.usersRepository.update(id, userData);
-        return {Success: true, statusCode: 200, message: '회원 수정이 완료되었습니다.'}
+        return {Success: true, statusCode: 200, message: '회원 수정이 완료되었습니다.'};
     }
 
     // 회원 탈퇴
     async delete(id: number) {
         await this.usersRepository.delete(id);
-        return {Success: true, statusCode: 200, message: '회원 삭제가 완료되었습니다.'}
+        return {Success: true, statusCode: 200, message: '회원 삭제가 완료되었습니다.'};
     }
 
     // 회원 조회(로그인 인증용)
