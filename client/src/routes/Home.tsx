9351a592f99aaf6b056b9b6b449263c9977b14d6
앵가리 <>
2023-02-27 23:24:48 +0900

    로그인 회원 인증 기능

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const access_token = localStorage.getItem('access_token');
+        axios.get('http://localhost:5000/auth', {
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
+        }).then(res => {
+            if(res.data.userId) {
+                console.log(res.data)
+            } else {
+                navigate('/login')
+            }
+        }).catch(Error => {
+            console.log(Error)
+            navigate('/login')
+        })
+    }, [])
+    
+
+    return (
+        <div>Home</div>
+    )
 }
 
 export default Home
