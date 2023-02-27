8e7dd1bb34145072268a0f724ce66088006f2518
앵가리 <>
2023-02-27 23:24:28 +0900

    token 만료시간 변경

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { UsersModule } from 'src/users/users.module';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '15m' },
     }),
   ],
   controllers: [AuthController],
