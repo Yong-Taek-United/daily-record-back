65067dba2374b097ee9b17b0a5c0d44f9d493cc2
앵가리 <>
2023-02-28 20:13:56 +0900

    auth response 값 추가

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -21,7 +21,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { email: user.email, sub: user.id };
+    const payload = { email: user.email, username:user.username, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
