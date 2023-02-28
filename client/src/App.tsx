1f71a5da9c02005ca627954eba54feb29740985e
앵가리 <>
2023-02-28 21:05:26 +0900

    컴포넌트 추가: NavBar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const Home = lazy(() => import('./routes/Home'));
 const SignUp = lazy(() => import('./routes/SignUp'));
 const Login = lazy(() => import('./routes/Login'));
+const NavBar = lazy(() => import('./components/NavBar'));
 
 const App = () => {
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
+        <NavBar />
         <Routes>
           <Route path="/*" element={<Home />}/>
           <Route path="/signup" element={<SignUp />}/>
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { AppBar,Box, Toolbar, Typography, Button, IconButton, } from '@mui/material';
+import { AddCircle } from '@mui/icons-material';
+import { api } from '../utils/authInstance';
+
+type ServerData = {
+    userData: {
+        userId: number;
+        email: string;
+        username: string;
+    }
+}
+
+const NavBar = () => {
+
+    const [UserId, setUserId] = useState(0);
+    const [Username, setUsername] = useState('');
+    
+
+    useEffect(() => {
+        const access_token = localStorage.getItem('access_token');
+
+        api().get<ServerData>('/auth', {
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
+        }).then(res => {
+            console.log(res.data.userData)
+            if(res.data) {
+                setUserId(res.data.userData.userId);
+                setUsername(res.data.userData.username);
+            }
+        }).catch(Error => {
+            console.log(Error)
+        });
+      
+    }, [])
+    
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                하루기록
+            </Typography>
+            {UserId ?
+                <div>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <AddCircle />
+                    </IconButton>
+                    <Button color="inherit">{Username}</Button>
+                </div>
+            :
+                <Button color="inherit">Login</Button>
+            }
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default NavBar;
