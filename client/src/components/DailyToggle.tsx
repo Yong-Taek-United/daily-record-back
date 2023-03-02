55d0c9b02d296d723968cc29f6dae6f12e81895a
앵가리 <>
2023-03-02 22:19:26 +0900

    데일리 수정 기능

--- client/src/components/DailyToggle.tsx
+++ client/src/components/DailyToggle.tsx
@@ -1,4 +1,4 @@
-import { KeyboardEvent, MouseEvent, useEffect, useState } from 'react';
+import { ChangeEvent, KeyboardEvent, MouseEvent, useEffect, useState } from 'react';
 import { Box, Button, Drawer } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/reducers/rootReducer';
@@ -21,13 +21,22 @@ function DailyToggle(props: Tprops) {
     const navigate = useNavigate();
 
     const [CurrDate, setCurrDate] = useState<Dayjs | null>(null);
-    
+    // const [EventText, setEventText] = useState('');
+    const [EventDescs, setEventDescs] = useState<string[]>([]);
+
+    const {CurrUserData} = useSelector((state: RootState) => state.userReducer);
     const {openCloseValue, CurDailyData} = useSelector((state: RootState) => state.dailyReducer);
     
     useEffect(() => {
         if(CurDailyData) {
             const date = dayjs(CurDailyData.date)
             setCurrDate(date)
+
+            if(CurDailyData.events) {
+                CurDailyData.events.map((event, i) => {
+                    setEventDescs(EventDescs => [...EventDescs, event.description]);
+                });
+            }
         }
     }, [CurDailyData])
 
@@ -40,13 +49,14 @@ function DailyToggle(props: Tprops) {
             )) {
             return;
             }
-            setCurrDate(null)
+            setEventDescs([]);
+            setCurrDate(null);
             setOpenToggle(open);
     };
 
     const createDaily = () => {
         let body = {
-            users: 1,
+            users: CurrUserData?.id,
             date: CurrDate
         };
         api().post('/dailies', body)
@@ -58,6 +68,27 @@ function DailyToggle(props: Tprops) {
         });
     };
 
+    const updateDaily = () => {
+        let body = {
+            users: CurrUserData?.id,
+            date: CurrDate
+        };
+        api().patch(`/dailies/${CurDailyData?.id}`, body)
+        .then(res => {
+            navigate('/')
+            console.log(res.data);
+        }).catch(Error => {
+            console.log(Error);
+        });
+    };
+
+    // const onEventHandle = (e:ChangeEvent<HTMLInputElement>) => {
+    //     setEventText(e.currentTarget.value)
+    // }
+
+    // const createEvent = () => {
+    //     setEventDescs(EventDescs => [...EventDescs, EventText]);
+    // };
     
     return (
         <Drawer
@@ -80,13 +111,25 @@ function DailyToggle(props: Tprops) {
                         renderInput={(params) => <TextField {...params} />}
                     />
                 </LocalizationProvider>
-                <p>{CurDailyData?.id}</p>
                 <div>
-                    {CurDailyData?.events?.map((event, i) => {
+                    {/* {CurDailyData?.events?.map((event, i) => {
                         return (<p key={i}>{event.description}</p>)
+                    })} */}
+                    {EventDescs.map((Desc, i) => {
+                        return (<p key={i}>{Desc}</p>)
                     })}
                 </div>
+                {/* <TextField
+                    id="eventText"
+                    placeholder='입력해주세요.'
+                    multiline
+                    maxRows={4}
+                    onChange={onEventHandle}
+                />
+                <Button onClick={createEvent}>작성</Button>
+                <br/> */}
                 <Button onClick={createDaily}>생성</Button>
+                <Button onClick={updateDaily}>수정</Button>
             </Box>
         </Drawer>
     );
