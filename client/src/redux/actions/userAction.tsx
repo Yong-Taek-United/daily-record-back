7ffc539947fe155f016b633c4fad5a58357905d7
앵가리 <>
2023-05-22 01:49:27 +0900

    client 분리

--- client/src/redux/actions/userAction.tsx
+++
@@ -1,7 +0,0 @@
-import * as type from '../types';
-
-export const SET_USERDATA = 'setUserData/SET_USERDATA' as const;
-export const setUserData = (user: type.userData) => ({
-    type: SET_USERDATA,
-    payload: user,
-});
