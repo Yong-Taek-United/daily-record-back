bd1d11d3467b535e28b9f5d307fe119170f28384
앵가리 <>
2023-06-28 23:26:25 +0900

    swagger 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +13,15 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Daily Record')
+    .setDescription('Project "Daily Record" API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
