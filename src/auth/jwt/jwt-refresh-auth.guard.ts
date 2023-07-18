e72522102f3f923ed62367003e1ea8f35048d642
앵가리 <>
2023-07-19 00:50:19 +0900

    auth 개선: Token 인증 에러 메세지 커스텀

--- src/auth/jwt/jwt-refresh-auth.guard.ts
+++ src/auth/jwt/jwt-refresh-auth.guard.ts
@@ -1,5 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtRefreshAuthGuard extends AuthGuard('jwt-refresh') {}
+export class JwtRefreshAuthGuard extends AuthGuard('jwt-refresh') {
+  handleRequest(err, user, info, context) {
+    if (err || !user) {
+      switch (info.name) {
+        case 'TokenExpiredError':
+          throw new UnauthorizedException('인증 토큰이 만료되었습니다.');
+          break;
+        case 'JsonWebTokenError':
+          throw new UnauthorizedException('인증 토큰이 유효하지 않습니다.');
+          break;
+        default:
+          throw new UnauthorizedException('Unauthorized');
+          break;
+      }
+    }
+    return user;
+  }
+}
