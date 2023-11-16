50a31a1fbb5c4b2a9452bd48bd41e40c96bd818c
앵가리 <>
2023-11-16 19:29:24 +0900

    로그인 인증 조건 추가: 회원가입 이메일 인증

--- src/shared/strategies/local.strategy.ts
+++ src/shared/strategies/local.strategy.ts
@@ -12,11 +12,16 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(email: string, password: string): Promise<any> {
+  async validate(email: string, password: string) {
     const user = await this.authService.validateUser(email, password);
-    if (!user) {
-      throw new UnauthorizedException('로그인 정보를 다시 확인해주세요.');
-    }
+    this.userIsNotVerified(user);
     return user;
+  }
+
+  private userIsNotVerified(user: any): void {
+    if (!user) throw new UnauthorizedException('로그인 정보를 다시 확인해주세요.');
+
+    if (!user.isEmailVerified)
+      throw new UnauthorizedException('회원가입 이메일 인증이 아직 완료되지 않았습니다. 이메일을 확인해주세요.');
   }
 }
