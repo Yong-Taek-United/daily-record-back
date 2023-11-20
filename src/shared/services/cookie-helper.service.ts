035aac5fa37d520769fa07016e26ae56a577bb7d
앵가리 <>
2023-11-21 02:38:55 +0900

    환경변수 처리: Cookie option

--- src/shared/services/cookie-helper.service.ts
+++ src/shared/services/cookie-helper.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { CookieOptions, Response } from 'express';
 
 @Injectable()
 export class CookieHelperService {
-  constructor() {}
+  constructor(private readonly configService: ConfigService) {}
 
   // 토큰 Cookie 저장
   async saveTokensToCookies(res: Response, tokens: any) {
     const option: CookieOptions = {
-      httpOnly: false,
-      sameSite: 'strict',
-      secure: false,
+      httpOnly: this.configService.get<boolean>('COOKIE_OPTION_HTTP_ONLY'),
+      sameSite: this.configService.get<CookieOptions['sameSite']>('COOKIE_OPTION_SAME_SITE'),
+      secure: this.configService.get<boolean>('COOKIE_OPTION_SECURE'),
     };
 
     Object.entries(tokens).forEach(([key, value]) => {
