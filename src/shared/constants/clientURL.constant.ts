976f6f34db0dad06d6295a3fd0219e3b9f68db65
앵가리 <>
2024-01-03 19:01:34 +0900

    chore: 프론트엔드 리디렉트 주소 변경

--- src/shared/constants/clientURL.constant.ts
+++ src/shared/constants/clientURL.constant.ts
@@ -2,10 +2,10 @@
 export const SIGN_UP_GOOGLE_URL = '/sign-up/social/google';
 
 // 이메일 인증 완료 페이지
-export const EMAIL_VERIFICATION_URL = '/email-verification/success';
+export const EMAIL_VERIFICATION_URL = '/email/verification/success';
 
 // 이메일 인증 실패 페이지
-export const EMAIL_VERIFICATION_FAILURE_URL = '/email-verification/failure';
+export const EMAIL_VERIFICATION_FAILURE_URL = '/email/verification/failure';
 
 // 비밀번호 재설정 페이지
-export const PASSWORD_RESET_URL = '/reset-password';
+export const PASSWORD_RESET_URL = '/reset/password';
