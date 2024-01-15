9821fcca839d1ee323aed86c18dd7994ebbdf24d
앵가리 <>
2024-01-16 00:49:14 +0900

    feat: 컬러 생성 및 수정, 조회, 비활성화

--- src/app/color/color.module.ts
+++ src/app/color/color.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ColorController } from './color.controller';
 import { ColorService } from './color.service';
+import { Color } from 'src/shared/entities/color.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Color])],
   controllers: [ColorController],
-  providers: [ColorService]
+  providers: [ColorService],
 })
 export class ColorModule {}
--- src/app/color/color.service.ts
+++ src/app/color/color.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Color } from 'src/shared/entities/color.entity';
+import { CreateColorDto, UpdateColorDto } from 'src/shared/dto/color.dto';
 
 @Injectable()
-export class ColorService {}
+export class ColorService {
+  constructor(
+    @InjectRepository(Color)
+    private readonly colorRepository: Repository<Color>,
+  ) {}
+
+  // 컬러 생성
+  async createColor(createData: CreateColorDto) {
+    const data = await this.colorRepository.save(createData);
+    return { statusCode: 201, data };
+  }
+  // 컬러 목록 조회
+  async getColorList() {
+    const data = await this.colorRepository.find({ where: { isActive: true } });
+    return { statusCode: 200, data };
+  }
+  // 컬러 수정
+  async updateColor(colorId: number, updateData: UpdateColorDto) {
+    const colorInfo = { ...updateData, id: colorId };
+    const data = await this.colorRepository.save(colorInfo);
+    return { statusCode: 200, data };
+  }
+  // 컬러 비활성화
+  async deactivateColor(colorId: number) {
+    const data = await this.colorRepository.update(colorId, { isActive: false });
+    return { statusCode: 200, data };
+  }
+}
