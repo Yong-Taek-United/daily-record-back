abb69794318c123adbccc7c59c699bbae43e4c40
앵가리 <>
2024-01-24 18:59:04 +0900

    fix: Interceptor 오퍼레이터 수정

--- src/shared/interceptors/api-response.interceptor.ts
+++ src/shared/interceptors/api-response.interceptor.ts
@@ -1,7 +1,7 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class ApiResponseInterceptor implements NestInterceptor {
@@ -9,7 +9,7 @@ export class ApiResponseInterceptor implements NestInterceptor {
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
-      map((response) => {
+      tap((response) => {
         const httpResponse = context.switchToHttp().getResponse();
 
         const { statusCode, message, data, redirect } = response;
